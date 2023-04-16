ffee7980dc3ab613d04a5e397750175b016c7e22
dchf <dchf.jp@gmail.com>
2023-04-17 00:15:11 +0900

    feat:Goによるクライアント実装

--- https_server.go
+++ https_server.go
@@ -19,6 +19,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	log.Println("start http listening :18443")
-	err := http.ListenAndServeTLS(":18443", "server.crt", "server.key", nil)
+	err := http.ListenAndServeTLS("localhost:18443", "server.crt", "server.key", nil)
 	log.Println(err)
 }
--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
-	resp, err := http.Get("https://example.com")
+	// 証明書を読み込む
+	cert, err := ioutil.ReadFile("ca.crt")
+	if err != nil {
+		panic(err)
+	}
+	certPool := x509.NewCertPool()
+	certPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{
+		RootCAs: certPool,
+	}
+	tlsConfig.BuildNameToCertificate()
+
+	// クライアントを作成
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	// 通信実行
+	resp, err := client.Get("https://localhost:18443")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	dump, err := httputil.DumpResponse(resp, true)
 	if err != nil {
 		panic(err)
 	}
-
-	log.Println(string(body))
+	log.Println(string(dump))
 }
